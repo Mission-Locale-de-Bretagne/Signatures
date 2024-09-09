a91a1610823e64703e6e8e305fb930150034c710
VMARIEARMLB <164041382+VMARIEARMLB@users.noreply.github.com>
2024-09-09 12:25:01 +0200

    Update 22DIN - generateur_signatures.ps1
    Modification pour prise en compte des propriétés AzureAD

--- 22DIN/22DIN - generateur_signatures.ps1
+++ 22DIN/22DIN - generateur_signatures.ps1
@@ -1,15 +1,15 @@
-﻿#Necessite que le roaming soit desactive :
+#Necessite que le roaming soit desactive :
 #Set-OrganizationConfig -PostponeRoamingSignaturesUntilLater:$true 
 
 Connect-ExchangeOnline
 
 
 # Cible le ou les utilisateurs concernÃ©s
-$users = Get-User -Filter {UserPrincipalName -like "vmarie@mlfougeres.onmicrosoft.com" -and RecipientTypeDetails -eq 'UserMailbox'} | Select-Object firstname,lastname,title,phone,mobilephone,userprincipalname,streetaddress,postalcode,city,Office,Company
+$users = Get-User -Filter {UserPrincipalName -like "vmarie@mlfougeres.onmicrosoft.com" -and RecipientTypeDetails -eq 'UserMailbox'} | Select-Object firstname,lastname,title,phone,mobilephone,userprincipalname,streetaddress,postalcode,city,Office,Company,department
 #$CompanyName = Get-AzureADUser -Filter "UserPrincipalName eq 'vmarie@mlfougeres.onmicrosoft.com'" | Select-Object -ExpandProperty CompanyName
 
 # Chemin vers le template HTML
-$templateSignatureHTML = Get-Content -Path "C:\Users\VincentMARIE\OneDrive - ARMLB\Documents\WindowsPowerShell\Scripts\Signatures\22DIN-template-signature.html" -raw
+$templateSignatureHTML = Get-Content -Path "C:\Users\VincentMARIE\OneDrive - ARMLB\Documents\WindowsPowerShell\Scripts\Pack Signature HTML\Signatures\22DIN-PAEJ-template-signature.html" -raw
 
 # Boucle pour chaque utilisateur
 foreach ($user in $users) { 
@@ -18,6 +18,8 @@ foreach ($user in $users) {
 	if ($user.firstname) { 
 		# RÃ©Ã©criture de l'adresse pour harmonisation
         if ($user.company -eq "Mission Locale du Pays de Dinan")
+        ## POUR PAEJ ##
+        # if ($user.department -eq "PAEJ")
         {
             #$building = "Immeuble .."
             $address = "Mission Locale du Pays de Dinan"
@@ -38,10 +40,10 @@ foreach ($user in $users) {
 		$signatureHTML = $signatureHTML.Replace("{Title}", $user.title) 
 		$signatureHTML = $signatureHTML.Replace("{Address}", $address)
         $SignatureHTML = $signatureHTML.Replace("{Building}",$building)
-		$signatureHTML = $signatureHTML.Replace("{Street}", $street) 
-		$signatureHTML = $signatureHTML.Replace("{PostalCode}", $postalcode) 
-		$signatureHTML = $signatureHTML.Replace("{City}", $city)  
-		$signatureHTML = $signatureHTML.Replace("{Phone}", $phone)  
+		$signatureHTML = $signatureHTML.Replace("{Street}", $user.streetaddress) 
+		$signatureHTML = $signatureHTML.Replace("{PostalCode}", $user.postalcode) 
+		$signatureHTML = $signatureHTML.Replace("{City}", $user.city)  
+		$signatureHTML = $signatureHTML.Replace("{Phone}", $usephone)  
 		$signatureHTML = $signatureHTML.Replace("{MobilePhone}", $user.mobilephone)
 
 	} 
