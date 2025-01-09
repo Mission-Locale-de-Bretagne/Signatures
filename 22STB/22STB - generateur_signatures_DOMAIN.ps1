3dcb8800044309abed90b358ddc66d068e65d818
Batiste DONDOGLIO <167748197+BatisteDONDOGLIO@users.noreply.github.com>
2025-01-09 12:21:53 +0100

    Update 22STB - generateur_signatures_DOMAIN.ps1

--- 22STB/22STB - generateur_signatures_DOMAIN.ps1
+++ 22STB/22STB - generateur_signatures_DOMAIN.ps1
@@ -1,24 +1,26 @@
-﻿#Necessite que le roaming soit desactive :
-#Set-OrganizationConfig -PostponeRoamingSignaturesUntilLater:$true 
+# Nécessite que le roaming soit desactivé :
+# Set-OrganizationConfig -PostponeRoamingSignaturesUntilLater:$true 
+
+# Nécessite le module ExchangeOnlineManagement ver. = 3.5.1
 
 Connect-ExchangeOnline
 
-
-# Cible le ou les utilisateurs concernÃ©s
-$users = Get-ExoMailBox -Filter {UserPrincipalName -like "*@mlstbrieuc.fr" -and RecipientTypeDetails -eq 'UserMailbox' -and CustomAttribute15 -eq "22STB"} | Select-Object firstname,lastname,title,phone,mobilephone,userprincipalname,streetaddress,postalcode,city,Office,Company
-#$CompanyName = Get-AzureADUser -Filter "UserPrincipalName eq 'vmarie@mlfougeres.onmicrosoft.com'" | Select-Object -ExpandProperty CompanyName
+# Cible le ou les utilisateurs concernés
+$mailboxes = Get-ExoMailBox -Filter {UserPrincipalName -like "*@mlstbrieuc.fr" -and RecipientTypeDetails -eq 'UserMailbox' -and CustomAttribute15 -eq "22STB"} | Select-Object UserPrincipalName
 
 # Chemin vers le template HTML
-$templateSignatureHTML = Get-Content -Path "C:\Users\VincentMARIE\OneDrive - ARMLB\Documents\WindowsPowerShell\Scripts\Signatures\22STB-template-signature.html" -raw
+$templateSignatureHTML = Get-Content -Path "<CHEMIN_VERS_TEMPLATE>\22STB-template-signature.html" -raw
 
 # Boucle pour chaque utilisateur
-foreach ($user in $users) { 
-	$signatureHTML = $templateSignatureHTML 
-	# VÃ©rification qu'il s'agit bien d'un utilisateur
-	if ($user.firstname) { 
-		# RÃ©Ã©criture de l'adresse pour harmonisation
-        if ($user.company -eq "Mission Locale du Pays de Saint-Brieuc")
+foreach ($mailbox in $mailboxes) { 
+    $user = Get-User -Identity $mailbox.UserPrincipalName | Select-Object FirstName, LastName, Title, Phone, MobilePhone, UserPrincipalName, StreetAddress, PostalCode, City, Office, Company
+    $signatureHTML = $templateSignatureHTML 
+    # Vérification qu'il s'agit bien d'un utilisateur
+    if ($user.FirstName) { 
+        # Réécriture de l'adresse pour harmonisation
+        if ($user.Company -eq "Mission Locale du Pays de Saint-Brieuc")
         {
+
             #$building = "Immeuble .."
             $address = "Mission Locale du Pays de Saint-Brieuc"
             $street = "47 Rue du docteur Rahuel"
@@ -27,29 +29,27 @@ foreach ($user in $users) {
             $phone = "02 96 68 15 68" 
 
             Write-Host "Utilisateur trouvé"
-
         } else {
-            Write-Host ("Erreur, aucune adresse ne correspond pour : {0} {1}" -f $user.firstname, $user.lastname)
-			exit
+            Write-Host ("Erreur, aucune adresse ne correspond pour : {0} {1}" -f $user.FirstName, $user.LastName)
+			continue
         }
 		# Remplacement des tags dans le template par les valeurs correspondantes
 		$signatureHTML = $signatureHTML.Replace("{First name}", $user.firstname) 
 		$signatureHTML = $signatureHTML.Replace("{Last name}", $user.lastname) 
 		$signatureHTML = $signatureHTML.Replace("{Title}", $user.title) 
 		$signatureHTML = $signatureHTML.Replace("{Address}", $address)
-        $SignatureHTML = $signatureHTML.Replace("{Building}",$building)
+        	#$SignatureHTML = $signatureHTML.Replace("{Building}",$building)
 		$signatureHTML = $signatureHTML.Replace("{Street}", $user.streetaddress) 
 		$signatureHTML = $signatureHTML.Replace("{PostalCode}", $user.postalcode) 
 		$signatureHTML = $signatureHTML.Replace("{City}", $user.city)  
 		$signatureHTML = $signatureHTML.Replace("{Phone}", $user.phone)  
 		$signatureHTML = $signatureHTML.Replace("{MobilePhone}", $user.mobilephone)
 
-	} 
+	    Write-Host ("Mise en place de la signature de : {0} {1}" -f $user.FirstName, $user.LastName)
+
+        # Mise en place de la signature sur le compte
+        Set-MailboxMessageConfiguration -Identity $user.UserPrincipalName -signatureHTML $signatureHTML -AutoAddSignature $true -AutoAddSignatureOnReply $true 
+    }
 }
 
-	Write-Host ("Mise en place de la signature de : {0} {1}" -f $user.firstname, $user.lastname)
-
-	# Mise en place de la signature sur le compte
-	Set-MailboxMessageConfiguration -Identity $user.userPrincipalName -signatureHTML $signatureHTML -AutoAddSignature $true -AutoAddSignatureOnReply $true 
-
-Disconnect-ExchangeOnline
+Disconnect-ExchangeOnline -Confirm:$false
